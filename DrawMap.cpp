673b0ffc953002eac726152ee30ea59a27b22038
israel-gs <israelgs83@gmai.com>
2024-06-20 00:53:50 -0500

    change map drawing

--- DrawMap.cpp
+++ DrawMap.cpp
@@ -4,101 +4,48 @@
 #include "Headers/Global.hpp"
 #include "Headers/DrawMap.hpp"
 
-void draw_map(const std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& i_map, sf::RenderWindow& i_window)
+void draw_map(const std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH> &i_map, sf::RenderWindow &i_window)
 {
-	sf::Sprite sprite;
-
-	sf::Texture texture;
-	texture.loadFromFile("Resources/Images/Map" + std::to_string(CELL_SIZE) + ".png");
-
-	sprite.setTexture(texture);
+	sf::RectangleShape wall_shape(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+	sf::RectangleShape door_shape(sf::Vector2f(CELL_SIZE, CELL_SIZE / 4));
+	sf::CircleShape pellet_shape(CELL_SIZE / 8);
+	sf::CircleShape energizer_shape(CELL_SIZE / 4);
 
 	for (unsigned char a = 0; a < MAP_WIDTH; a++)
 	{
 		for (unsigned char b = 0; b < MAP_HEIGHT; b++)
 		{
-			sprite.setPosition(static_cast<float>(CELL_SIZE * a), static_cast<float>(CELL_SIZE * b));
+			wall_shape.setPosition(static_cast<float>(CELL_SIZE * a), static_cast<float>(CELL_SIZE * b));
 
-			//We just crop out what we need from the texture.
 			switch (i_map[a][b])
 			{
-				case Cell::Door:
-				{
-					sprite.setTextureRect(sf::IntRect(2 * CELL_SIZE, CELL_SIZE, CELL_SIZE, CELL_SIZE));
-
-					i_window.draw(sprite);
-
-					break;
-				}
-				case Cell::Energizer:
-				{
-					sprite.setTextureRect(sf::IntRect(CELL_SIZE, CELL_SIZE, CELL_SIZE, CELL_SIZE));
-
-					i_window.draw(sprite);
-
-					break;
-				}
-				case Cell::Pellet:
-				{
-					sprite.setTextureRect(sf::IntRect(0, CELL_SIZE, CELL_SIZE, CELL_SIZE));
-
-					i_window.draw(sprite);
-
-					break;
-				}
-				case Cell::Wall:
-				{
-					bool down = 0;
-					bool left = 0;
-					bool right = 0;
-					bool up = 0;
-
-					if (b < MAP_HEIGHT - 1)
-					{
-						if (Cell::Wall == i_map[a][1 + b])
-						{
-							down = 1;
-						}
-					}
-
-					//Since we have warp tunnels, we need to draw them as if they're connected.
-					if (0 < a)
-					{
-						if (Cell::Wall == i_map[a - 1][b])
-						{
-							left = 1;
-						}
-					}
-					else
-					{
-						left = 1;
-					}
-
-					if (a < MAP_WIDTH - 1)
-					{
-						if (Cell::Wall == i_map[1 + a][b])
-						{
-							right = 1;
-						}
-					}
-					else
-					{
-						right = 1;
-					}
-
-					if (0 < b)
-					{
-						if (Cell::Wall == i_map[a][b - 1])
-						{
-							up = 1;
-						}
-					}
-
-					//--------------------------------------------<         DISTRIBUTIVE PROPERTY!         >----------------------------
-					sprite.setTextureRect(sf::IntRect(CELL_SIZE * (down + 2 * (left + 2 * (right + 2 * up))), 0, CELL_SIZE, CELL_SIZE));
-
-					i_window.draw(sprite);
-				}
+			case Cell::Door:
+			{
+				door_shape.setFillColor(sf::Color::White);
+				door_shape.setPosition(static_cast<float>(CELL_SIZE * a), static_cast<float>(CELL_SIZE * b));
+				i_window.draw(door_shape);
+				break;
+			}
+			case Cell::Energizer:
+			{
+				energizer_shape.setFillColor(sf::Color::White);
+				energizer_shape.setPosition(static_cast<float>(CELL_SIZE * a + (CELL_SIZE / 2 - energizer_shape.getRadius())), static_cast<float>(CELL_SIZE * b + (CELL_SIZE / 2 - energizer_shape.getRadius())));
+				i_window.draw(energizer_shape);
+				break;
+			}
+			case Cell::Pellet:
+			{
+				pellet_shape.setFillColor(sf::Color::White);
+				pellet_shape.setPosition(static_cast<float>(CELL_SIZE * a + (CELL_SIZE / 2 - pellet_shape.getRadius())), static_cast<float>(CELL_SIZE * b + (CELL_SIZE / 2 - pellet_shape.getRadius())));
+				i_window.draw(pellet_shape);
+				break;
+			}
+			case Cell::Wall:
+			{
+				wall_shape.setFillColor(sf::Color::Blue);
+				i_window.draw(wall_shape);
+				break;
+			}
 			}
 		}
 	}
